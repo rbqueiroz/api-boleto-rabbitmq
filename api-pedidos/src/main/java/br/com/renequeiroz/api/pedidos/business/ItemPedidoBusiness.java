cbc6f994d6c3dcbb8f72ee25d17cd9e1c576c095
Rene Queiroz <rene.queiroz@cearaprev.ce.gov.br>
2024-12-24 16:19:12 -0300

    classe pedido, business, repository

---
+++ api-pedidos/src/main/java/br/com/renequeiroz/api/pedidos/business/ItemPedidoBusiness.java
@@ -0,0 +1,4 @@
+package br.com.renequeiroz.api.pedidos.business;
+
+public interface ItemPedidoBusiness {
+}
---
+++ api-pedidos/src/main/java/br/com/renequeiroz/api/pedidos/repository/ItemPedidoRepository.java
@@ -0,0 +1,12 @@
+package br.com.renequeiroz.api.pedidos.repository;
+
+import br.com.renequeiroz.api.pedidos.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
+    List<ItemPedido> findByPedidoId(Long pedidoId);
+}
