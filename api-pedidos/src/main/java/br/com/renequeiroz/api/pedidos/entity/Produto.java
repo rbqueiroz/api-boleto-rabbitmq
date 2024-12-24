0b3b2caa3f2119918ee49d3d9945fa04cec8d7a1
Rene Queiroz <rene.queiroz@cearaprev.ce.gov.br>
2024-12-24 14:49:10 -0300

    classe produto

---
+++ api-pedidos/src/main/java/br/com/renequeiroz/api/pedidos/entity/Produto.java
@@ -0,0 +1,25 @@
+package br.com.renequeiroz.api.pedidos.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "valor")
+    private Double valor;
+}
