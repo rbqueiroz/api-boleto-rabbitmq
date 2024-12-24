b056c412149c52559376c9e170356b7a291f052c
Rene Queiroz <rene.queiroz@cearaprev.ce.gov.br>
2024-12-24 16:18:00 -0300

    class, business, repository

---
+++ api-pedidos/src/main/java/br/com/renequeiroz/api/pedidos/business/Impl/ProdutoBusinessImpl.java
@@ -0,0 +1,8 @@
+package br.com.renequeiroz.api.pedidos.business.Impl;
+
+import br.com.renequeiroz.api.pedidos.business.ProdutoBusiness;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoBusinessImpl implements ProdutoBusiness {
+}
--- api-pedidos/src/main/java/br/com/renequeiroz/api/pedidos/entity/Produto.java
+++ api-pedidos/src/main/java/br/com/renequeiroz/api/pedidos/entity/Produto.java
@@ -1,15 +1,7 @@
 package br.com.renequeiroz.api.pedidos.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class Produto {
 
@@ -22,4 +14,36 @@ public class Produto {
 
     @Column(name = "valor")
     private Double valor;
+
+    public Produto() {
+    }
+    public Produto(Long id, String nome, Double valor) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
 }
