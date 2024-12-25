2382fcab9493df3971010409c0791017398b0255
Rene Queiroz <rene.queiroz@cearaprev.ce.gov.br>
2024-12-25 15:25:31 -0300

    update pedido controller

--- api-pedidos/src/main/java/br/com/renequeiroz/api/pedidos/controller/PedidoController.java
+++ api-pedidos/src/main/java/br/com/renequeiroz/api/pedidos/controller/PedidoController.java
@@ -1,8 +1,11 @@
 package br.com.renequeiroz.api.pedidos.controller;
 
+import br.com.renequeiroz.api.pedidos.business.PedidoBusiness;
+import br.com.renequeiroz.api.pedidos.dto.PedidoCriadoDTO;
 import br.com.renequeiroz.api.pedidos.entity.Pedido;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +18,19 @@ public class PedidoController {
 
     private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
 
+    private final PedidoBusiness business;
+
+    public PedidoController(PedidoBusiness business) {
+        this.business = business;
+    }
+
     @PostMapping
-    public ResponseEntity<Pedido> cricarPedido(@RequestBody Pedido pedido) {
-        logger.info("Criando pedido {}", pedido);
-        return ResponseEntity.ok(pedido);
+    public ResponseEntity<PedidoCriadoDTO> cricarPedido(@RequestBody Pedido pedido) {
+        PedidoCriadoDTO pedidoCriadoDTO = business.processarPedido(pedido);
+        if(pedidoCriadoDTO == null) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        logger.info("Novo pedido feito{}", pedido);
+        return ResponseEntity.ok(pedidoCriadoDTO);
     }
 }
