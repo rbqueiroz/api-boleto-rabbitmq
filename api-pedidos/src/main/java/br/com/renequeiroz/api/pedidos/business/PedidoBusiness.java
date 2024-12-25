a4c3b7f602e8a4234ee7162665b0c1f0a596712e
Rene Queiroz <rene.queiroz@cearaprev.ce.gov.br>
2024-12-25 15:24:28 -0300

    metodo processar pedido

--- api-pedidos/src/main/java/br/com/renequeiroz/api/pedidos/business/PedidoBusiness.java
+++ api-pedidos/src/main/java/br/com/renequeiroz/api/pedidos/business/PedidoBusiness.java
@@ -1,4 +1,9 @@
 package br.com.renequeiroz.api.pedidos.business;
 
+import br.com.renequeiroz.api.pedidos.dto.PedidoCriadoDTO;
+import br.com.renequeiroz.api.pedidos.entity.Pedido;
+import br.com.renequeiroz.api.pedidos.exceptions.ErrorAoSalvarException;
+
 public interface PedidoBusiness {
+    PedidoCriadoDTO processarPedido(Pedido pedido) throws ErrorAoSalvarException;
 }
